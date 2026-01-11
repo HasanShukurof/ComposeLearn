207326040b01aa9070b090da4ab62a1f662db236
Hasan Shukurov <hasan.shukurof@gmail.com>
2026-01-11 21:35:53 +0400

    learn compose

--- app/src/main/java/com/example/mcpse/MainActivity.kt
+++ app/src/main/java/com/example/mcpse/MainActivity.kt
@@ -4,46 +4,57 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.snapping.SnapPosition
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.mcpse.ui.theme.MCpseTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+     //   enableEdgeToEdge()
         setContent {
-            MCpseTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            Row(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.LightGray),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                Text("Hello")
+                Text("Again Hello")
+                Text("World")
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier,
-        color = Color.Red
-    )
-}
+//@Composable
+//fun Greeting(name: String) {
+//    Text(
+//        text = "Hello $name!",
+//    )
+//}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MCpseTheme {
-        Greeting("Android")
-    }
-}
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    MCpseTheme {
+//        Greeting("Android")
+//    }
+//}
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,38 +1,7 @@
-//// Top-level build file where you can add configuration options common to all sub-projects/modules.
-//plugins {
-//    alias(libs.plugins.android.application) apply false
-//    alias(libs.plugins.kotlin.android) apply false
-//    alias(libs.plugins.kotlin.compose) apply false
-//}
-
-val kotlinVersion = "2.1.10"
-
-buildscript {
-//    extra.apply {
-//        set("kotlin_version", "2.1.10")
-//        set("hilt_version", "2.55")
-//    }
-    repositories {
-        google()
-//        jcenter()
-        mavenCentral()
-    }
-    dependencies {
-        classpath (libs.gradle)
-        classpath (libs.kotlin.gradle.plugin)
-        // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle files
-        classpath (libs.google.services)
-        classpath (libs.androidx.navigation.safe.args.gradle.plugin)
-        classpath (libs.firebase.crashlytics.gradle)
-
-    }
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.compose) apply false
 }
-
-//plugins {
-//    id ("org.jlleitschuh.gradle.ktlint" version "7.1.0")
-//    id 'com.google.dagger.hilt.android' version '2.43.2' apply false
-//    id 'org.jetbrains.kotlin.android' version '2.1.10' apply false
-//}
-
 
